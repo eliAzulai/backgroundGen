648f804328c1e91c652cd7aaddfd306eb88ab186
eliAzulai <81955327+eliAzulai@users.noreply.github.com>
2025-02-17 14:27:20 +0200

    new sctipts

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20]
+
+array.sort(function(a, b){return a - b});
+console.log(array)
+// make a function
+function answer(array) {
+	const mapArray = array.map((num,i) => {
+		return num
+	})
+} 
+// iterate over the array, decide which type probably map
+// method that identifies identical type 
+toSorted
+// create new array for each distinct type 
+if array[i] === array[i+1]
+// add each distinct type into its own array 
+
